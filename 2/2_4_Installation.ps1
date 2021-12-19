4705da84d7e33f5cb18d6d0089f87a5cb9aa5d18
philiplorenz-code <84645793+philiplorenz-code@users.noreply.github.com>
2021-12-19 15:07:23 -0800

    init

---
+++ 2/2_4_Installation.ps1
@@ -0,0 +1,15 @@
+Install-PackageProvider Nuget -Force
+Install-Module AutomatedLab -AllowClobber
+
+# If you are on Linux and are not starting pwsh with sudo
+# This needs to executed only once per user - adjust according to your needs!
+Set-PSFConfig -Module AutomatedLab -Name LabAppDataRoot -Value /home/youruser/.alConfig -PassThru | Register-PSFConfig
+
+# Prepare sample content - modify to your needs
+
+# Windows
+New-LabSourcesFolder -Drive C
+
+# Linux
+Set-PSFConfig -Module AutomatedLab -Name LabSourcesLocation -Value /home/youruser/labsources -PassThru | Register-PSFConfig
+New-LabSourcesFolder # Linux
